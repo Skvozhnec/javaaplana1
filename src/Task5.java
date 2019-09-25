c65cbf2b6ba7c8c9674035ce136657134463971d
Roman <Skvozhnec@gmail.com>
2019-09-26 00:03:22 +0300

    таблица умножения

---
+++ src/Task5.java
@@ -0,0 +1,11 @@
+/*Необходимо вывести на экран таблицу умножения от 1 до 10 для введённого пользователем любого числа. Использовать циклы запрещено.*/
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        String number = sc.nextLine();
+    }
+}

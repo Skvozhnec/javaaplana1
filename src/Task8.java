bc2c9ecc5d04fb4dafaf32362fc95594dd1fa729
Roman <Skvozhnec@gmail.com>
2019-10-15 01:05:58 +0300

    Задание 8. Вывод чисел от 1 до 50.

---
+++ src/Task8.java
@@ -0,0 +1,11 @@
+/* Необходимо с помощью цикла вывести на экран числа от 1 до 50. */
+
+
+public class Task8 {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 50; i++) {
+            System.out.print(i + ", ");
+        }
+    }
+}
+
